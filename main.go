e9bbe50463fc8f212106555ac56c15a8bfc650d6
Mihajlo Kocić <kocicmihajlo@gmail.com>
2018-04-07 21:54:49 +0200

    fix indentation add structs

--- main.go
+++ main.go
@@ -1,22 +1,37 @@
 package main
 
 import (
-  "net/http"
-  "strings"
+	"net/http"
+	"strings"
 )
 
-func handleHttp(w http.ResponseWriter, r *http.Request) {
-  message := r.URL.Path
-  message = strings.TrimPrefix(message, "/")
-  message = "Hello " + message
+type FlightInfo struct {
+	unlock_cost int32
+	param_value int32
+	is_unlocked bool
+}
 
-  w.Write([]byte(message))
+type GameState struct {
+	prev_country_ids  []byte
+	last_timestamp    int64
+	score             int32
+	current_country   byte
+	countries_guessed []byte
+	cities_received   []FlightInfo
+}
+
+
+func handleHttp(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Path
+	message = strings.TrimPrefix(message, "/")
+	message = "Hello " + message
+
+	w.Write([]byte(message))
 }
 
 func main() {
-  http.HandleFunc("/", handleHttp)
-  if err := http.ListenAndServe(":8080", nil); err != nil {
-    panic(err)
-  }
+	http.HandleFunc("/", handleHttp)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 }
-
